9a4d639d57f0de7ff1880f095499b182a3177fc5
Gianni P <perkinsgianni@gmail.com>
2022-05-05 11:45:35 -0400

    Clean up syntax in app.jsx

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -4,7 +4,7 @@ export default class App extends React.Component {
   // initialize state
   constructor(props) {
     super(props);
-    
+
     this.state = {
       balance: '',
       rate: '',
@@ -43,14 +43,14 @@ export default class App extends React.Component {
   calculatePayment(balance, rate, term) {
     const r = rate / 100 / 12;
     const n = term * 12;
-    const numerator = r * (1 + r) ** n;
-    const denominator = (1 + r) ** n - 1;
+    const numerator = (r * (1 + r)) ** n;
+    const denominator = (1 + r) ** (n - 1);
 
     return parseFloat(balance * (numerator / denominator)).toFixed(2);
   }
 
   // amortizationSchedule(balance, rate, term) {
-  //   let table = 
+  //   const table =
   //     `<tr>
   //       <th>Month</th>
   //       <th>Payment</th>
@@ -65,11 +65,11 @@ export default class App extends React.Component {
   handleClick(e) {
     e.preventDefault();
 
-    let balance = this.state.balance;
-    let rate = this.state.rate;
-    let term = this.state.term;
-    let payment = this.calculatePayment(balance, rate, term);
-   
+    const balance = this.state.balance;
+    const rate = this.state.rate;
+    const term = this.state.term;
+    const payment = this.calculatePayment(balance, rate, term);
+
     // update state value
     this.setState({
       payment: `$${payment} is your monthly payment.`
@@ -78,75 +78,75 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <form className="form-horizontal">
-          <div className="col-md-2" />
+      <div className='container'>
+        <form className='form-horizontal'>
+          <div className='col-md-2' />
 
           {/* header */}
-          <div className="page-header">
+          <div className='page-header'>
             <h3>Mortgage Calculator</h3>
           </div>
 
           {/* balance */}
-          <div className="form-group">
-            <label htmlFor="balance" className="col-md-2 control-label">Loan Balance</label>
-            <div className="col-md-5">
-              <input 
-                name="balance" 
-                value={this.state.balance} 
-                onChange={this.updateBalance} 
-                placeholder="0" 
-                className="form-control input-md" 
-                size="1"
+          <div className='form-group'>
+            <label htmlFor='balance' className='col-md-2 control-label'>Loan Balance</label>
+            <div className='col-md-5'>
+              <input
+                name='balance'
+                value={ this.state.balance }
+                onChange={ this.updateBalance }
+                placeholder='0'
+                className='form-control input-md'
+                size='1'
               />
             </div>
           </div>
 
           {/* rate */}
-          <div className="form-group">
-            <label htmlFor="rate" className="col-md-2 control-label">
+          <div className='form-group'>
+            <label htmlFor='rate' className='col-md-2 control-label'>
               Interest Rate (%)
             </label>
-            <div className="col-md-5">
-              <input 
-                name="rate" 
-                step={0.01} 
-                value={this.state.rate} 
-                onChange={this.updateRate}
-                placeholder="0"
-                className="form-control input-md"
-                size="1"
+            <div className='col-md-5'>
+              <input
+                name='rate'
+                step={ 0.01 }
+                value={ this.state.rate }
+                onChange={ this.updateRate }
+                placeholder='0'
+                className='form-control input-md'
+                size='1'
               />
             </div>
           </div>
 
           {/* loan term */}
-          <div className="form-group">
-            <label htmlFor="term" className="col-md-2 control-label">
+          <div className='form-group'>
+            <label htmlFor='term' className='col-md-2 control-label'>
               Loan Term (years)
             </label>
-            <div className="col-md-5">
-              <select 
-                name="term"
-                type="number" 
-                value={this.state.term} 
-                onChange={this.updateTerm}
-                className="form-control"
+            <div className='col-md-5'>
+              <select
+                name='term'
+                type='number'
+                value={ this.state.term }
+                onChange={ this.updateTerm }
+                className='form-control'
               >
                 <option>--Please select a loan term--</option>
-                <option value="15">15</option>
-                <option value="30">30</option>
+                <option value='15'>15</option>
+                <option value='30'>30</option>
               </select>
             </div>
           </div>
 
           {/* calculate button */}
-          <div className="form-group">
-            <div className="col-md-offset-2 col-md-10">
-              <button 
-                name="submit" 
-                onClick={this.handleClick}
-                className="btn btn-primary"
+          <div className='form-group'>
+            <div className='col-md-offset-2 col-md-10'>
+              <button
+                name='submit'
+                onClick={ this.handleClick }
+                className='btn btn-primary'
               >
                 Calculate
               </button>
@@ -154,7 +154,7 @@ export default class App extends React.Component {
           </div>
 
           {/* output display */}
-          <div id="output" name="output" className="d-print-inline-block">
+          <div id='output' name='output' className='d-print-inline-block'>
             {this.state.payment}
           </div>
         </form>
